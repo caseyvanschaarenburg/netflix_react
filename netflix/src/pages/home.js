355b11851c4e6b9b39e25189c28577d2c2deab24
casey van schaarenburg <caseyschaarenburg@gmail.com>
2021-04-12 11:04:16 +0200

    header

--- netflix/src/pages/home.js
+++ netflix/src/pages/home.js
@@ -3,11 +3,25 @@ import {JumbotronContainer} from "../containers/jumbotron";
 import {FaqsContainer} from "../containers/faqs";
 import {FooterContainer} from "../containers/footer";
 import {BrowserRouter as Router} from "react-router-dom";
+import {HeaderContainer} from "../containers/header";
+import { Feature, OptForm } from '../components';
 
 export default function Home() {
     return <>
-        <JumbotronContainer />
-        <FaqsContainer />
-        <FooterContainer/>
+        <HeaderContainer>
+            <Feature>
+                <Feature.Title>Unlimited films, TV programmes and more.</Feature.Title>
+                <Feature.SubTitle>Watch anywhere. Cancel at any time.</Feature.SubTitle>
+                <OptForm>
+                    <OptForm.Input placeholder="Email address" />
+                    <OptForm.Button>Try it now</OptForm.Button>
+                    <OptForm.Break />
+                    <OptForm.Text>Ready to watch? Enter your email to create or restart your membership.</OptForm.Text>
+                </OptForm>
+            </Feature>
+        </HeaderContainer>
+            <JumbotronContainer />
+            <FaqsContainer />
+            <FooterContainer/>
     </>
 }
