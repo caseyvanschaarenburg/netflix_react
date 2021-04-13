f07ca48b171559d37188cb93815086b59c23a092
casey van schaarenburg <caseyschaarenburg@gmail.com>
2021-04-13 13:31:10 +0200

    small change

--- netflix/src/containers/browse.js
+++ netflix/src/containers/browse.js
@@ -99,7 +99,7 @@ export function BrowseContainer({ slides }) {
                         <Card.Feature category={category}>
                             <Player>
                                 <Player.Button />
-                                <Player.Video src="/videos/bunny.mp4" />
+                                <Player.Video src="/video/bunny.mp4" />
                             </Player>
                         </Card.Feature>
                     </Card>
